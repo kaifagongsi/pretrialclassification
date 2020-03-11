d84460350d4af11b593952a30bb05270cd4d3604
lsy <923372823@qq.com>
2020-03-11 14:57:51 +0800

    提交测试

--- src/main/java/com/kfgs/pretrialclassification/dao/FenleiBaohuMainMapper.java
+++ src/main/java/com/kfgs/pretrialclassification/dao/FenleiBaohuMainMapper.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.kfgs.pretrialclassification.domain.FenleiBaohuMain;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
