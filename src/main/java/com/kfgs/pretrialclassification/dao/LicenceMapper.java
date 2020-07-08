c4e06413636386dcc1207808a86f74d040503edb
mangoL-KFGS <mango_liang@outlook.com>
2020-07-08 13:58:07 +0800

    遗漏

---
+++ src/main/java/com/kfgs/pretrialclassification/dao/LicenceMapper.java
@@ -0,0 +1,7 @@
+package com.kfgs.pretrialclassification.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kfgs.pretrialclassification.domain.Licence;
+
+public interface LicenceMapper extends BaseMapper<Licence> {
+}
