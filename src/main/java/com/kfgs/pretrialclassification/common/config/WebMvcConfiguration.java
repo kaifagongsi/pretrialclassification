6c39f80809ca012d29785309ef94eafeb775b569
mangoL-KFGS <mango_liang@outlook.com>
2020-07-08 09:39:33 +0800

    修改swaggerui的配置

--- src/main/java/com/kfgs/pretrialclassification/common/config/WebMvcConfiguration.java
+++ src/main/java/com/kfgs/pretrialclassification/common/config/WebMvcConfiguration.java
@@ -2,9 +2,11 @@ package com.kfgs.pretrialclassification.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -32,6 +34,23 @@ public class WebMvcConfiguration  implements WebMvcConfigurer {
         corsConfiguration.setMaxAge(Long.parseLong("168000"));
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        /**
+         * SpringBoot自动配置本身并不会把/swagger-ui.html
+         * 这个路径映射到对应的目录META-INF/resources/下面
+         * 采用WebMvcConfigurerAdapter将swagger的静态文件进行发布;
+         */
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        //将所有/static/** 访问都映射到classpath:/static/ 目录下
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX +"/static/");
+        //super.addResourceHandlers(registry);
     }
 
 }
