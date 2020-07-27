ea13dab9098bd927c47b40e8c86b2b654f342a69
mango_liang <mango_liang@outlook.com>
2020-07-27 17:04:15 +0800

    处理申请号更正列表

---
+++ src/main/java/com/kfgs/pretrialclassification/dao/FenleiBaohuUpdateIpcMapper.java
@@ -0,0 +1,11 @@
+package com.kfgs.pretrialclassification.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kfgs.pretrialclassification.domain.FenleiBaohuUpdateipc;
+
+/**
+ * @author mango
+ */
+public interface FenleiBaohuUpdateipcMapper extends BaseMapper<FenleiBaohuUpdateipc> {
+
+}
---
+++ src/main/java/com/kfgs/pretrialclassification/updateIpc/controller/FenleiBaohuUpdateipcController.java
@@ -0,0 +1,14 @@
+package com.kfgs.pretrialclassification.updateIpc.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mango
+ */
+@RestController
+@RequestMapping("/gen/fenlei-baohu-updateipc")
+public class FenleiBaohuUpdateipcController {
+
+}
