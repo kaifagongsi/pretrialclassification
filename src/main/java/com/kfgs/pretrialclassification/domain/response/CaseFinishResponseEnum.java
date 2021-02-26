2c426674bacbf5fb3ce01cda00476c09bf0f4be7
mango_liang <mango_liang@outlook.com>
2021-02-26 16:42:43 +0800

    修正多打个文字 ： 个

--- src/main/java/com/kfgs/pretrialclassification/domain/response/CaseFinishResponseEnum.java
+++ src/main/java/com/kfgs/pretrialclassification/domain/response/CaseFinishResponseEnum.java
@@ -16,7 +16,7 @@ public enum CaseFinishResponseEnum implements ResultCode {
     FM_NO_CCI(false,24003,"发明案件的的CPC分类号不能为空，需要裁决"),
     NO_CLASSIFICATION(false,24004,"无分类号"),
     NO_IPCMI_ONE_IPCOI(false,24005,"无主分有一个分类员给副分"),
-    NO_IPCMI_MORE_IPCOI(false,24006,"无主分有多个个分类员给副分"),
+    NO_IPCMI_MORE_IPCOI(false,24006,"无主分有多个分类员给副分"),
     NO_IPCMI_NO_IPCOI_ONE_IPCA(false,24007,"无主分无副分仅有一个附加信息"),
     NO_IPCMI_NO_IPCOI_MORE_IPCA(false,24008,"无主分无副分有多个附加信息"),
     EXPORT_FINISH(false,24009,"案件已完成导出，不可再提出更改"),
