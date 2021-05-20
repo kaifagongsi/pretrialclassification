d3740495f7c695bceddd68b6ebaeb36c09a7ec04
mango_liang <mango_liang@outlook.com>
2021-05-20 14:49:19 +0800

    添加部门信息

--- src/main/java/com/kfgs/pretrialclassification/common/service/impl/ReadisInitService.java
+++ src/main/java/com/kfgs/pretrialclassification/common/service/impl/ReadisInitService.java
@@ -29,8 +29,8 @@ public class ReadisInitService  implements CommandLineRunner {
         HashMap map = new HashMap(10);
         map.put("一部",new String[]{"一室","二室","三室"});
         map.put("二部",new String[]{"一室","二室","三室"});
-        map.put("三部",new String[]{"一室","二室","三室","四部","五部"});
-        map.put("四部",new String[]{"一室","二室","三室","四部"});
+        map.put("三部",new String[]{"一室","二室","三室","四室","五室"});
+        map.put("四部",new String[]{"一室","二室","三室","四室"});
         map.put("专利业务研究与发展部",new String[]{"计划管理室","质量保障室","规划与发展研究室"});
         map.put("系统建设与运维部",new String[]{"项目计划与研究室","数据及网络管理室","项目建设及运营室","运行维护室"});
         System.out.println("向redis中写入部门数据...");
