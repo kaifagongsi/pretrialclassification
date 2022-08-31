9431313c1a715bdba5d584b2d21eb4e94f4202e5
mango_liang <mango_liang@outlook.com>
2022-08-31 10:23:12 +0800

    裁决备份表 service

---
+++ src/main/java/com/kfgs/pretrialclassification/caseArbiter/service/IFenleiBaohuAdjuInforBackupService.java
@@ -0,0 +1,11 @@
+package com.kfgs.pretrialclassification.caseArbiter.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.kfgs.pretrialclassification.domain.FenleiBaohuAdjuInforBackup;
+
+/**
+ * @author mango
+ */
+public interface IFenleiBaohuAdjuInforBackupService extends IService<FenleiBaohuAdjuInforBackup> {
+
+}
---
+++ src/main/java/com/kfgs/pretrialclassification/caseArbiter/service/impl/FenleiBaohuAdjuInforBackupServiceImpl.java
@@ -0,0 +1,16 @@
+package com.kfgs.pretrialclassification.caseArbiter.service.impl;
+
+import com.kfgs.pretrialclassification.dao.FenleiBaohuAdjuInforBackupMapper;
+import com.kfgs.pretrialclassification.domain.FenleiBaohuAdjuInforBackup;
+import com.kfgs.pretrialclassification.caseArbiter.service.IFenleiBaohuAdjuInforBackupService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author mango
+ */
+@Service
+public class FenleiBaohuAdjuInforBackupServiceImpl extends ServiceImpl<FenleiBaohuAdjuInforBackupMapper, FenleiBaohuAdjuInforBackup> implements IFenleiBaohuAdjuInforBackupService {
+
+
+}
