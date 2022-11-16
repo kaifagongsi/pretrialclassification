a42af0f37f1d9c5f3ee5a0aa1548c67550e5f94a
mango_liang <mango_liang@outlook.com>
2022-11-16 09:14:50 +0800

    添加保护中心表：FenleiBaohuResultChangeBack

---
+++ src/main/java/com/kfgs/pretrialclassification/dao/FenleiBaohuResultChangBackMapper.java
@@ -0,0 +1,13 @@
+package com.kfgs.pretrialclassification.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kfgs.pretrialclassification.domain.FenleiBaohuResultChangBack;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author mango
+ */
+@Repository
+public interface FenleiBaohuResultChangBackMapper extends BaseMapper<FenleiBaohuResultChangBack> {
+
+}
